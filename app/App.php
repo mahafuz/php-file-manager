d82771e42748718974dd9ab54e5e90ed0f0e150f
mahafuz <m.mahfuz.me@gmail.com>
2021-01-26 20:52:29 +0600

    Initial Commit:

---
+++ app/App.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App;
+
+
+class App {
+	public function __construct(  ) {
+		echo "Hello World";
+	}
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new \App\App();
